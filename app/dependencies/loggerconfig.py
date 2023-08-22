1859e5d36e9e987e29413438ff1505816bfd3a49
wildb <wildbillhodges@gmail.com>
2023-08-22 07:39:13 -0400

    initial

---
+++ app/dependencies/loggerconfig.py
@@ -0,0 +1,57 @@
+import logging.config
+import logging as log
+from app.dependencies import constants
+
+logger = log.getLogger()
+
+
+config = {   
+        
+    "logging": {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "single-line": {
+                "class": "logging.Formatter",
+                "style": "{",
+                "datefmt": "%I:%M:%S",
+                "format": "{levelname:}: {message:s}"
+            }
+        },
+        "handlers": {
+            "console":{
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "single-line",
+                "stream" : "ext://sys.stdout"
+            }
+        },
+        "loggers": { },
+        "root": {
+            "handlers": ["console"],
+            "level": constants.DEFAULT_LOG_LEVEL
+        }
+    }      
+}
+
+# --------------------------------------------------
+
+def configure_logger (level: str):
+    """ Configures logging with the json above and inserts the 
+        passed level
+    """
+  
+    valid_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+    # If the level is supplied
+    if (level is not None and isinstance(level, str) and level in valid_levels):
+        config["logging"]["root"]["level"] = level
+        # Configure the logger
+        logging.config.dictConfig(config['logging'])
+    else:
+        # Configure the logger
+        logging.config.dictConfig(config['logging']) 
+        logger.error("Invalid logger level given. Using default")   
+            
+    
+    
