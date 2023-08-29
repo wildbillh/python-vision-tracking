0dfad12324f2c1bb9e470c26df1eac8f912ab65b
wildb <wildbillhodges@gmail.com>
2023-08-29 17:48:49 -0400

    example use

---
+++ selection-tool/__main__.py
@@ -0,0 +1,19 @@
+
+import cv2
+import numpy as np
+from typing import Tuple
+from app.dependencies.videoshow import SelectionVideoShow
+from app.dependencies.utils import removeROIs
+
+# Exercise the SelectionVideoShow class 
+
+def main():
+    
+	#vs = SelectionVideoShow(interimProcessFunc = removeROIs)
+	vs = SelectionVideoShow()
+	
+	image = cv2.imread("selection-tool/81.jpg")
+	print("returns", vs.show(image, processFunc=removeROIs)[1], flush=True)
+
+
+main()
