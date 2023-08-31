51cd8c92f9db04158935a566904919c5272ae649
wildb <wildbillhodges@gmail.com>
2023-08-31 06:55:23 -0400

    add generateNegativeFile

--- training/__main__.py
+++ training/__main__.py
@@ -1,6 +1,7 @@
 
 import argparse, logging, sys, traceback
 from app.dependencies.loggerconfig import configure_logger
+from app.dependencies.utils import generateNegFile
 from training.dependencies.extractnegatives import ExtractNegatives
 from training.dependencies.extractpositives import ExtractPositives
 
@@ -9,7 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument (
         'program',
         type = str,
-        choices = ['extract-negatives', 'extract-positives']
+        choices = ['extract-negatives', 'extract-positives', 'generate-negative-file']
     )
 
 args = vars(parser.parse_args(sys.argv[1:]))
@@ -26,16 +27,24 @@ class Main:
 
         logger.info(f'Executing: {args["program"]}')
 
+        # Generate Negative images. Selected areas are copied from left-most area
         if args["program"] == "extract-negatives":
-            extract_negatives = ExtractNegatives("clips/kb/fr-trans2.mp4", "fr-trans2", 
+            extract_negatives = ExtractNegatives("clips/kb/good-jump.mp4", "good-jump", 
                                              "training/kb/clips/negative")
             extract_negatives.run(processSize=[1440, 810], finishedSize=[960,540]) 
         
+        # Generate positive images and pos.txt file
         elif args["program"] == "extract-positives":
-            extract_positives = ExtractPositives("clips/kb/fr-trans2.mp4", "fr-trans2", 
+            extract_positives = ExtractPositives("clips/kb/good-jump.mp4", "good-jump", 
                                              "training/kb/clips/positive",
-                                             "training/kb/pos.txt")
-            extract_positives.run(processSize=[1440, 810], finishedSize=[960,540])
+                                             "training/kb/pos.txt")    
+            extract_positives.run(processSize=[1680, 945], finishedSize=[960,540])
+
+        # Generate the neg.txt file base on the given folders
+        elif args["program"] == "generate-negative-file":
+            generateNegFile(["training/kb/clips/negative"], "training/kb/neg.txt")
+
+
 
 if __name__ == "__main__":
     try:
